3cea31d9fc592cabf26e28536dc86e1105cb1600
Justin Reich <justinreich.dev@gmail.com>
2019-02-06 16:44:58 -0600

    add second part of JS with HTML exercise

--- js/external.js
+++ js/external.js
@@ -2,3 +2,9 @@
 
 
 console.log("Hello from external JavaScript");
+
+alert('Welcome to my Website!');
+
+var userColor = prompt("Enter your favorite color!");
+
+alert(userColor + " " + "is my favorite color too!!");
