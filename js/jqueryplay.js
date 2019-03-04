1cc5e35f2bdd8c8e0953a8535a11775ae14f2dd0
Daniel Fryar <daniel.fryar@gmail.com>
2019-03-04 13:30:15 -0600

    end of css and attribute lesson

--- js/jqueryplay.js
+++ js/jqueryplay.js
@@ -1,12 +1,13 @@
 "use strict";
 $(document).ready(function() {
 
+    $('#btn4').click(function() {
+        console.log($('#panel4 .panel-heading').toggleClass("underlined"));
+        console.log($('#panel4 .panel-body').toggleClass("highlighted"));
+    });
 
-
-
-
-
-
-
+    $('#btn1').click(function() {
+        $('#panel1').toggleClass('hidden');
+    });
 });
 
