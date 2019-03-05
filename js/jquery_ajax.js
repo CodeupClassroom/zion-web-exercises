6451a3284f159acff8c49f1a5a4f354d695254cc
Daniel Fryar <daniel.fryar@gmail.com>
2019-03-05 11:59:38 -0600

    We are done with AJAX lesson

--- js/jquery_ajax.js
+++ js/jquery_ajax.js
@@ -3,51 +3,45 @@
 $().ready(function() {
 
     var url = "https://swapi.co/api/people/";
-    var planeturl = "https://swapi.co/api/planets/";
-    // for (var i = 1; i < 88; i++) {
-    //     var response = $.ajax(url + i + '/');
-    //     response.done(function(data, status, res) {
-    //         console.log(data.url + ' = ' + data.name );
-    //     });
-    //     response.fail(function (req, status, error) {
-    //         console.log(req);
-    //     });
-    //     response.always(function() {
-    //         console.log(i);
-    //     })
-    // }
+
+
+    function onSuccess(data, status, req) {
+        $('#panel1').css('opacity', 0.5);
+        $('#panel1').removeClass('panel-danger').addClass('panel-primary');
+        $('#panel1 .panel-heading').html(data.name);
+        // console.log(data);
+        $('#hair').html(
+            "hair color: " + data.hair_color);
+        var planetreq = $.ajax(data.homeworld);
+        planetreq.done(function (data2) {
+            $('#home').html(
+                "homeworld: " + data2.name
+            );
+        })
+
+    }
+
+    function onFail(req, status, error) {
+        $('#panel1').css('opacity', 0.5);
+        $('#panel1').removeClass('panel-primary').addClass('panel-danger');
+        $('#panel1 .panel-heading').html("Error!!");
+        $('#hair').html("These are not the droids you are looking for");
+        $('#home').html('');
+
+    }
+
+    function cleanup() {
+        $('#panel1').css('opacity', 1);
+
+    }
+
+
     $('#btn1').click(function(){
         var input = $('#input').val();
         var response = $.ajax(url + input);
-        response.done(function (data) {
-            $('#panel1 .panel-heading').html(data.name);
-            console.log(data);
-            $('#hair').html(
-                "hair color: " + data.hair_color);
-            var planetreq = $.ajax(data.homeworld);
-            planetreq.done(function (data2) {
-                $('#home').html(
-
-                    "homeworld: " + data2.name
-                );
-            })
-
-        });
+        response.done(onSuccess).fail(onFail).always(cleanup);
 
     })
-    //
-    // var response = $.ajax(url + '20');
-    //
-    // response.done(function (data, status, req) {
-    //     console.log(data.name);
-    //     console.log(status);
-    //     console.log(req);
-    // });
-    //
-    // response.fail(function () {
-    //     console.error("This is not the response you are looking for")
-    // });
-    // console.log(response.responseJSON.name);
 
 
 })
